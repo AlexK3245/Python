bc4a17cdacacf551b3ceb5b924505c03933a3542
AlexK3245 <akasper@students.mchenry.edu>
2024-08-26 14:40:45 -0500

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 10)
+
+# Integer
+y = 24
+print("Integer:", y)
+
+# Float
+z = 19.8
+print("Float:", z)
+
+# String
+name = "Alex"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
