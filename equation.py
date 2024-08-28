d6609ade57a3c1e703b1e6703d96fbd8aa311cc3
AlexK3245 <akasper@students.mchenry.edu>
2024-08-28 13:41:46 -0500

    Add files via upload

---
+++ equation.py
@@ -0,0 +1,7 @@
+# This code calculates the area of a rectangle and then prints it
+length = 15
+width = 8
+area = length * width
+
+print("The area of the rectangle has a length of " + str(length) +
+      " and a width of " + str(width) + " is " + str(area))
