ed8a5ab1e23708fb79ec561213fc3c7786912e8e
AlexK3245 <akasper@students.mchenry.edu>
2024-09-04 13:29:51 -0500

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,19 @@
+# Comment Assignment
+
+"""
+    Type the above code into Visual Studio Code
+    Add a minimum of 4 comments to the above Python 
+    program to make it more understandable.
+"""
+
+# The amount of gallons the tank can hold
+tank_size = 15
+
+# The distance in miles that the vehicle was able to travel on one tank
+distance = 600
+
+# This calculates the amount of miles the vehicle could drive per gallon
+mpg = distance / tank_size
+
+# This prints what the mpg ended up coming out to after calculating it
+print("Miles per gallon:", mpg)
