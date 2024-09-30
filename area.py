688648311f46b31cba658143e2e3635ced697310
AlexK3245 <akasper@students.mchenry.edu>
2024-09-30 13:24:38 -0500

    Add files via upload

---
+++ area.py
@@ -0,0 +1,16 @@
+
+# This function will calculate the area of a square
+def square(side):
+    area = side * side
+    print(f"The area of the square is {area} square units.")
+
+
+# This function will calculate the area of a circle
+def circle(radius):
+    area_cir = 3.14 * (radius * radius)
+    print(f"The area of the circle is {area_cir} sqaure units.")
+
+
+# Calls the functions that were defined above
+square(4)
+circle(5)
